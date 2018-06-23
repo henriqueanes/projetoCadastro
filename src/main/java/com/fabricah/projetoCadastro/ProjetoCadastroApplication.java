495d222e9604e984843977cfd61f16700d1ff15a
Henrique Anes <h.sousaanes@gmail.com>
2018-06-23 18:31:48 -0300

    Projeto criado

---
+++ src/main/java/com/fabricah/projetoCadastro/ProjetoCadastroApplication.java
@@ -0,0 +1,12 @@
+package com.fabricah.projetoCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroApplication.class, args);
+	}
+}

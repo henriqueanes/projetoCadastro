5777ff85aa59b2310f514b50d064a72b64953eb1
Henrique Anes <h.sousaanes@gmail.com>
2018-07-02 22:29:55 -0300

    Criando operação de instanciação

--- src/main/java/com/fabricah/projetoCadastro/ProjetoCadastroApplication.java
+++ src/main/java/com/fabricah/projetoCadastro/ProjetoCadastroApplication.java
@@ -1,12 +1,30 @@
 package com.fabricah.projetoCadastro;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoCadastroApplication {
+import com.fabricah.projetoCadastro.domain.Categoria;
+import com.fabricah.projetoCadastro.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoCadastroApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoCadastroApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
